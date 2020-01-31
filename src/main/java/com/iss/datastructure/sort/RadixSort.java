b0ffdd239535ac5a853749cdc36fe4cbd4e39ba9
Jingjie <jingjie>
2020-01-31 16:30:32 +0800

    算法

---
+++ src/main/java/com/iss/datastructure/sort/RadixSort.java
@@ -0,0 +1,35 @@
+package com.iss.datastructure.sort;
+
+public class RadixSort {
+    static void radixSort(int[] arr) {
+        int[][] bucket = new int[10][arr.length];
+        int[] bucketEleCounts = new int[10];
+
+        int max = arr[0];
+        for (int ele : arr) {
+            if (max < ele) {
+                max = ele;
+            }
+        }
+
+        int maxLen = String.valueOf(max).length();
+
+        for (int k = 0; k < maxLen; k++) {
+            for (int ele : arr) {
+                int digitOfEle = ele / (int) Math.pow(10, k) % 10;
+                bucket[digitOfEle][bucketEleCounts[digitOfEle]] = ele;
+                bucketEleCounts[digitOfEle] += 1;
+            }
+
+            int index = 0;
+            for (int i = 0; i < bucket.length; i++) {
+                if (bucketEleCounts[i] != 0) {
+                    for (int j = 0; j < bucketEleCounts[i]; j++) {
+                        arr[index++] = bucket[i][j];
+                    }
+                }
+                bucketEleCounts[i] = 0;
+            }
+        }
+    }
+}
---
+++ src/main/java/com/iss/datastructure/sort/SelectSort.java
@@ -0,0 +1,20 @@
+package com.iss.datastructure.sort;
+
+public class SelectSort {
+    static void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            int min = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (min > arr[j]) {
+                    minIndex = j;
+                    min = arr[j];
+                }
+            }
+            if (minIndex != i) {
+                arr[minIndex] = arr[i];
+                arr[i] = min;
+            }
+        }
+    }
+}
